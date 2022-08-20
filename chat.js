d3182391c68a8574c2d6062465752caf0207195c
Abhigyan Das <103254159+Abhigyan-Das@users.noreply.github.com>
2022-08-20 18:22:56 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -15,9 +15,9 @@ firebase.initializeApp(firebaseConfig);
 function addUser()
 {
     user_name = document.getElementById("user_name").value;
-    firebase.database().ref("/").child(user_name).update({
+    /*firebase.database().ref("/").child(user_name).update({
         purpose : "adding user"
-    });
+    });*/
     
     localStorage.setItem("user_name", user_name);
 
